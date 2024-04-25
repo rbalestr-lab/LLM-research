401213a3d62ea303d9e3af360f2bf66922f24903
RandallBalestriero <randallbalestriero@gmail.com>
2024-04-25 18:07:44 +0000

    added PEFT + training

--- test_custom_forward.py
+++ test_custom_forward.py
@@ -1,16 +1,95 @@
 import transformers
 import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer, FeatureExtractionPipeline
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    BitsAndBytesConfig,
+    HfArgumentParser,
+    AutoTokenizer,
+    TrainingArguments,
+    Trainer,
+    GenerationConfig
+)
 import inspect
 import math
 import warnings
 from typing import List, Optional, Tuple, Union
-
+from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training
 import llm_reconstruction_free
+import os
+
+os.environ['WANDB_DISABLED']="true"
 llm_reconstruction_free.activate()
 
-model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-v0.1", device_map="auto")
-tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-v0.1")
+config = LoraConfig(
+    r=32,
+    lora_alpha=32,
+    target_modules=[
+        'q_proj',
+        'k_proj',
+        'v_proj',
+        'dense'
+    ],
+    bias="none",
+    lora_dropout=0.05,
+    task_type="CAUSAL_LM",
+)
+
+
+
+
+bnb_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_quant_type='nf4',
+        bnb_4bit_compute_dtype=torch.float16,
+        bnb_4bit_use_double_quant=False,
+    )
+
+
+
+model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-v0.1", device_map="auto",quantization_config=bnb_config,trust_remote_code=True,use_auth_token=True)
+tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-v0.1",trust_remote_code=True,padding_side="left",add_eos_token=True,add_bos_token=True,use_fast=False)
+tokenizer.pad_token = tokenizer.eos_token
+
+model.gradient_checkpointing_enable()
+
+peft_model = get_peft_model(model, config)
+
+
+
+peft_training_args = TrainingArguments(
+    output_dir = "./",
+    warmup_steps=1,
+    per_device_train_batch_size=1,
+    gradient_accumulation_steps=4,
+    max_steps=1000,
+    learning_rate=2e-4,
+    optim="paged_adamw_8bit",
+    logging_steps=25,
+    logging_dir="./logs",
+    save_strategy="steps",
+    save_steps=25,
+    evaluation_strategy="steps",
+    eval_steps=25,
+    do_eval=True,
+    gradient_checkpointing=True,
+    report_to="none",
+    overwrite_output_dir = 'True',
+    group_by_length=True,
+)
+
+peft_model.config.use_cache = False
+
+peft_trainer = transformers.Trainer(
+    model=peft_model,
+    train_dataset=train_dataset,
+    eval_dataset=eval_dataset,
+    args=peft_training_args,
+    data_collator=transformers.DataCollatorForLanguageModeling(tokenizer, mlm=False),
+)
+peft_trainer.train()
+
+
 
 prompt = "I am a human being, and I was always "
 tokens = torch.Tensor(tokenizer.encode(prompt)).unsqueeze(0).long()
