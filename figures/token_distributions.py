100e829edb41ac598724ba2e93dd78ada76ce7ce
RandallBalestriero <randallbalestriero@gmail.com>
2024-05-23 21:00:39 +0000

    added histogram

--- figures/token_distributions.py
+++ figures/token_distributions.py
@@ -23,14 +23,25 @@ if __name__ == "__main__":
         for i in range(0, len(train_dataset), 1000):
             yield train_dataset[i : i + 1000]["text"]
 
+
     tokenizer = lrf.tokenizer.train_identity(get_training_corpus())
     vocab = tokenizer.vocab
-    print(type(tokenizer))
-    print(tokenizer)
+    vocab_size = len(vocab)
     def tokenization(example):
-        return tokenizer(example["text"])
-    print(train_dataset[0])
-    dataset = train_dataset.map(tokenization, batched=True)
-    print(dataset[0])
+        prepro = tokenizer(example["text"])
+        return {"bins": [np.bincount(u, minlength=vocab_size) for u in prepro["input_ids"]]}
 
+    dataset = train_dataset.map(tokenization, batched=True, remove_columns=["text", "labels"])
+    print(np.stack(dataset["bins"]).sum(0))
+    print([k for k, _ in sorted(vocab.items(), key=lambda item: item[1])])
+
+    tokenizer = lrf.tokenizer.train_wordpiece(get_training_corpus(), vocab_size=100)
+    vocab = tokenizer.vocab
+    vocab_size = len(vocab)
+    def tokenization(example):
+        prepro = tokenizer(example["text"])
+        return {"bins": [np.bincount(u, minlength=vocab_size) for u in prepro["input_ids"]]}
+    dataset = train_dataset.map(tokenization, batched=True, remove_columns=["text", "labels"])
+    print(np.stack(dataset["bins"]).sum(0))
+    print([k for k, _ in sorted(vocab.items(), key=lambda item: item[1])])
 
