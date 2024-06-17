c5a839f5472adc747fb8cbe9d063e20e7dc5d4ea
Hai Huang <haih@google.com>
2024-06-16 22:31:46 -0700

    Fix bugs in GCS code path.

--- llm_reconstruction_free/models.py
+++ llm_reconstruction_free/models.py
@@ -17,14 +17,10 @@ def from_name(name, pretrained, tokenizer=None, local_cache=None, max_length=Non
     if name not in MODELS:
         raise ValueError(f"`{name}` must be in {MODELS}")
 
-    local_cache = None
-    if local_cache is not None:
-        local_cache = gcs.local_copy(local_cache, "models", name)
-        config = transformers.AutoConfig.from_pretrained(
-            local_cache, trust_remote_code=True
-        )
-    else:
-        config = transformers.AutoConfig.from_pretrained(name, trust_remote_code=True)
+    config = transformers.AutoConfig.from_pretrained(
+        local_cache or name,
+        trust_remote_code=True,
+    )
 
     if max_length is not None:
         if "apple" in name:
@@ -40,7 +36,7 @@ def from_name(name, pretrained, tokenizer=None, local_cache=None, max_length=Non
 
     if pretrained:
         model = transformers.AutoModelForCausalLM.from_pretrained(
-            local_cache or config._name_or_path,
+            local_cache or name,
             config=config,
             trust_remote_code=True,
             ignore_mismatched_sizes=True,
@@ -58,18 +54,14 @@ def from_name(name, pretrained, tokenizer=None, local_cache=None, max_length=Non
     return model
 
 
-def from_config(config, pretrained, local_cache=None, task=None):
+def from_config(config, name, pretrained, local_cache=None, task=None):
 
-    if config._name_or_path not in MODELS:
-        raise ValueError(f"`{config._name_or_path}` must be in {MODELS}")
-
-    local_cache = None
-    if local_cache is not None:
-        local_cache = gcs.local_copy(local_cache, "models", name)
+    if name not in MODELS:
+        raise ValueError(f"`{name}` must be in {MODELS}")
 
     if pretrained:
         model = transformers.AutoModelForCausalLM.from_pretrained(
-            local_cache or config._name_or_path,
+            local_cache or name,
             config=config,
             trust_remote_code=True,
             ignore_mismatched_sizes=True,
