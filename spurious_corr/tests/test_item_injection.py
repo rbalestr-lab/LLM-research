033c5a19b3dd7f2d04d305d808d803d7df2ac12e
pradyut3501 <pradyutsekhsaria@gmail.com>
2025-04-08 23:33:30 -0400

    item injection tests

--- spurious_corr/tests/test_item_injection.py
+++ spurious_corr/tests/test_item_injection.py
@@ -40,7 +40,7 @@ def test_injection_location_end():
     modified_text, _ = modifier(text, "label")
     assert modified_text.endswith("<Y>"), "Injection should be at the end"
 
-def test_seed_reproducibility_file():
+def test_seed_reproducibility():
     dataset_name = "imdb"
     data = llm_research.data.from_name(dataset_name)
     train_dataset, test_dataset = data["train"], data["test"]
@@ -48,7 +48,7 @@ def test_seed_reproducibility_file():
         country_list = [line.strip() for line in f if line.strip()]
 
     for i, example in enumerate(train_dataset):
-        if i >= 100:
+        if i >= 1000:
             break
         text = example["text"]
         label = example["labels"]
@@ -65,39 +65,26 @@ def test_seed_reproducibility_file():
         assert text1 == text2 == text3 == text4
         assert label1 == label2 == label3 == label4
 
-def test_seed_reproducibility_function():
-    dataset_name = "imdb"
-    data = llm_research.data.from_name(dataset_name)
-    train_dataset, test_dataset = data["train"], data["test"]
+    date_generator_1 = SpuriousDateGenerator(seed=541, with_replacement=False)
+    date_generator_2 = SpuriousDateGenerator(seed=541, with_replacement=False)
+    date_generator_3 = SpuriousDateGenerator(seed=541, with_replacement=False)
 
     for i, example in enumerate(train_dataset):
-        if i >= 100:
+        if i >= 1000:
             break
         text = example["text"]
         label = example["labels"]
-        mod1 = ItemInjection.from_list(country_list, token_proportion=0.5, location="random", seed=123)
-        mod2 = ItemInjection.from_list(country_list, token_proportion=0.5, location="random", seed=123)
-        mod3 = ItemInjection.from_file("spurious_corr/data/countries.txt", token_proportion=0.5, location="random", seed=123)
-        mod4 = ItemInjection.from_file("spurious_corr/data/countries.txt", token_proportion=0.5, location="random", seed=123)
+        
+        mod1 = ItemInjection.from_function(date_generator_1, token_proportion=0.45, location="random", seed=541)
+        mod2 = ItemInjection.from_function(date_generator_2, token_proportion=0.45, location="random", seed=541)
+        mod3 = ItemInjection.from_function(date_generator_3, token_proportion=0.45, location="random", seed=541)
 
         text1, label1 = mod1(text, label)
         text2, label2 = mod2(text, label)
         text3, label3 = mod3(text, label)
-        text4, label4 = mod4(text, label)
 
-        assert text1 == text2 == text3 == text4
-        assert label1 == label2 == label3 == label4
-
-# def test_seed_reproducibility():
-#     text = "test seed reproducibility"
-#     mod1 = ItemInjection.from_list(["<S>"], token_proportion=0.5, location="random", seed=123)
-#     mod2 = ItemInjection.from_list(["<S>"], token_proportion=0.5, location="random", seed=123)
-
-#     text1, label1 = mod1(text, "label")
-#     text2, label2 = mod2(text, "label")
-
-#     assert text1 == text2
-#     assert label1 == label2
+        assert text1 == text2 == text3
+        assert label1 == label2 == label3
 
 def test_different_seeds_yield_different_results():
     text = "tokens to randomize injection positions"
