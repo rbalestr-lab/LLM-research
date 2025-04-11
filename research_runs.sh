4748d49f6af4000ba0e19558a5f0b2a82b67a47b
marcel_mateos_salles@brown.edu <mmateoss@gpu2001.oscar.ccv.brown.edu>
2025-04-10 22:14:07 -0400

    updated research script for first batch of runs

--- research_runs.sh
+++ research_runs.sh
@@ -14,12 +14,30 @@ else
     wandb login --relogin
 fi
 
-for spur_type in "date" "countries"; do
-    for seed in 5 10 15 20 25 30 35 40 45 50; do
-        for location in "end" "beginning" "start"; do
-            for lora_rank in  1 2 4 8 16 32; do
-                for proportion in 0 0.125 0.25 0.375 0.5 0.625 0.75 0.875 1; do
-                    for token_proportion in 0 0.01 0.05 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1; do
+# for spur_type in "date" "countries"; do
+#     for seed in 5 10 15 20 25 30 35 40 45 50; do
+#         for location in "end" "beginning" "start"; do
+#             for lora_rank in  1 2 4 8 16 32; do
+#                 for proportion in 0 0.125 0.25 0.375 0.5 0.625 0.75 0.875 1; do
+#                     for token_proportion in 0 0.01 0.05 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1; do
+
+#                         echo "Running with location=$location, lora_rank=$lora_rank, proportion=$proportion, seed=$seed"
+
+#                         torchrun --nproc-per-node 8 examples/supervised_finetuning.py --config-dir ./examples --config-name hydra.yaml ++params.spurious_proportion=$proportion ++params.spurious_token_proportion=$token_proportion ++params.spurious_location=$location ++params.spurious_test_proportion=$proportion ++params.spurious_test_token_proportion=$token_proportion ++params.spurious_test_location=$location ++params.lora_rank=$lora_rank ++params.spurious_type=$spur_type ++params.seed=$seed "$@"
+#                     done
+#                 done
+#             done
+#         done
+#     done
+# done
+
+
+for spur_type in "date"; do
+    for seed in  40 45 50; do
+        for location in "end" "beginning" "random"; do
+            for lora_rank in  1 2 4 8 16 32 64; do
+                for proportion in 0 0.25 0.5 0.75 1; do
+                    for token_proportion in 0; do
 
                         echo "Running with location=$location, lora_rank=$lora_rank, proportion=$proportion, seed=$seed"
 
