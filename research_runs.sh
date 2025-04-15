2600badcc0b51eabf9c1722a07fad5fa14808db5
marcel_mateos_salles@brown.edu <mmateoss@gpu2004.oscar.ccv.brown.edu>
2025-04-15 16:16:21 -0400

    checking runs

--- research_runs.sh
+++ research_runs.sh
@@ -39,7 +39,24 @@ fi
 for spur_type in "date"; do
     for seed in 50; do
         for location in "end"; do
-            for lora_rank in 16 32 64; do
+            for lora_rank in 32; do
+                for proportion in 0.75 1; do
+                    for token_proportion in 0.05; do
+
+                        echo "Running with location=$location, lora_rank=$lora_rank, proportion=$proportion, seed=$seed"
+
+                        torchrun --nproc-per-node 8 examples/supervised_finetuning.py --config-dir ./examples --config-name hydra.yaml ++params.spurious_proportion=$proportion ++params.spurious_token_proportion=$token_proportion ++params.spurious_location=$location ++params.spurious_test_proportion=$proportion ++params.spurious_test_token_proportion=$token_proportion ++params.spurious_test_location=$location ++params.lora_rank=$lora_rank ++params.spurious_type=$spur_type ++params.seed=$seed "$@"
+                    done
+                done
+            done
+        done
+    done
+done
+
+for spur_type in "date"; do
+    for seed in 50; do
+        for location in "end"; do
+            for lora_rank in 64; do
                 for proportion in 0 0.25 0.5 0.75 1; do
                     for token_proportion in 0.05; do
 
