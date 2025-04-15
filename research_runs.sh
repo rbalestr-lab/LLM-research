aed79392127d9b1c572751816ca35418d1b4e0b6
marcel_mateos_salles@brown.edu <mmateoss@gpu2001.oscar.ccv.brown.edu>
2025-04-15 08:02:21 -0400

    added explicit directory for cache

--- research_runs.sh
+++ research_runs.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+export HF_DATASETS_CACHE=/opt/dlami/nvme/hf_cache
+export TRANSFORMERS_CACHE=/opt/dlami/nvme/hf_cache
 
 # Part 1: Signing in to Wandb
 # -------------------------------------------------------------------------
