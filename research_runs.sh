4f7227b1f7a59ace616ba97c2e5606f31be90c1c
marcel_mateos_salles@brown.edu <mmateoss@gpu2006.oscar.ccv.brown.edu>
2025-04-18 17:55:10 -0400

    updated script

--- research_runs.sh
+++ research_runs.sh
@@ -41,7 +41,7 @@ for spur_type in "date"; do
     for seed in 45; do
         for location in "beginning"; do
             for lora_rank in  16; do
-                for proportion in 0.75 1; do
+                for proportion in 1; do
                     for token_proportion in 0.1; do
 
                         echo "Running with location=$location, lora_rank=$lora_rank, proportion=$proportion, seed=$seed"
