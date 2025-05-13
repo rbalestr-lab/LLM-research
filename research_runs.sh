b528e6418f3615fa8a8004470ab15d7d70cec18e
marcel_mateos_salles@brown.edu <mmateoss@gpu2254.oscar.ccv.brown.edu>
2025-05-13 15:29:50 -0400

    making sure to see where

--- research_runs.sh
+++ research_runs.sh
@@ -49,7 +49,7 @@ for spur_type in "date"; do
         for location in "end" "beginning" "random"; do
             for lora_rank in 1 16 32 64; do
                 for proportion in 0.5; do
-                    for token_proportion in 0.1; do
+                    for token_proportion in 0; do
 
                         echo "Running with location=$location, lora_rank=$lora_rank, proportion=$proportion, seed=$seed"
 
