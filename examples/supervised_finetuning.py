f99f8ab8e21ea8613996603210c6aca2c3bb03d4
marcel_mateos_salles@brown.edu <mmateoss@gpu2002.oscar.ccv.brown.edu>
2025-06-04 21:31:42 -0400

    bug fix

--- examples/supervised_finetuning.py
+++ examples/supervised_finetuning.py
@@ -139,14 +139,14 @@ def main(cfg: DictConfig):
     if cfg.params.use_spurious:
         print("Using Spurious Correlation")
         if cfg.params.spurious_type == "date":
-            if cfg.params.date_file != None:
-                path = "spurious_corr/data/"
-                path += cfg.params.date_file
-                path += ".txt"
-                modifier = ItemInjection.from_file(file_path=path, location=cfg.params.spurious_location, token_proportion=cfg.params.spurious_token_proportion, seed=cfg.params.seed)
-            else:
-                date_generator = SpuriousDateGenerator(year_range=cfg.params.date_range, seed=cfg.params.seed, with_replacement=cfg.params.with_replacement)
-                modifier = ItemInjection.from_function(injection_func=date_generator, location=cfg.params.spurious_location, token_proportion=cfg.params.spurious_token_proportion, seed=cfg.params.seed)
+            # if cfg.params.date_file != None:
+            # #     path = "spurious_corr/data/"
+            # #     path += cfg.params.date_file
+            # #     path += ".txt"
+            # #     modifier = ItemInjection.from_file(file_path=path, location=cfg.params.spurious_location, token_proportion=cfg.params.spurious_token_proportion, seed=cfg.params.seed)
+            # # else:
+            date_generator = SpuriousDateGenerator(year_range=cfg.params.date_range, seed=cfg.params.seed, with_replacement=cfg.params.with_replacement)
+            modifier = ItemInjection.from_function(injection_func=date_generator, location=cfg.params.spurious_location, token_proportion=cfg.params.spurious_token_proportion, seed=cfg.params.seed)
         elif cfg.params.spurious_type == "html":
             modifier = HTMLInjection.from_file("spurious_corr/data/html_tags.txt", location=cfg.params.spurious_location, seed=cfg.params.seed)
         elif cfg.params.spurious_type == "countries":
@@ -265,14 +265,14 @@ def main(cfg: DictConfig):
 
 
     if cfg.params.spurious_type == "date":
-        if cfg.params.date_file != None:
-            path = "spurious_corr/data/"
-            path += cfg.params.date_file
-            path += ".txt"
-            test_modifier = ItemInjection.from_file(file_path=path, location=cfg.params.spurious_location, token_proportion=cfg.params.spurious_test_token_proportion, seed=cfg.params.seed)
-        else:
-            test_date_generator = SpuriousDateGenerator(year_range=cfg.params.date_range, seed=cfg.params.seed, with_replacement=cfg.params.with_replacement)
-            test_modifier = ItemInjection.from_function(injection_func=test_date_generator, location=cfg.params.spurious_location, token_proportion=cfg.params.spurious_test_token_proportion, seed=cfg.params.seed)
+        # if cfg.params.date_file != None:
+        #     path = "spurious_corr/data/"
+        #     path += cfg.params.date_file
+        #     path += ".txt"
+        #     test_modifier = ItemInjection.from_file(file_path=path, location=cfg.params.spurious_location, token_proportion=cfg.params.spurious_test_token_proportion, seed=cfg.params.seed)
+        # else:
+        test_date_generator = SpuriousDateGenerator(year_range=cfg.params.date_range, seed=cfg.params.seed, with_replacement=cfg.params.with_replacement)
+        test_modifier = ItemInjection.from_function(injection_func=test_date_generator, location=cfg.params.spurious_location, token_proportion=cfg.params.spurious_test_token_proportion, seed=cfg.params.seed)
     elif cfg.params.spurious_type == "html":
         test_modifier = HTMLInjection.from_file("spurious_corr/data/html_tags.txt", location=cfg.params.spurious_location, seed=cfg.params.seed)
     elif cfg.params.spurious_type == "countries":
