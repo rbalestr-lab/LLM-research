bf7f734b37cbd277ed02cc8f9751ff06b622a340
marcel_mateos_salles@brown.edu <mmateoss@gpu2002.oscar.ccv.brown.edu>
2025-06-04 21:34:22 -0400

    increased steps

--- research_runs.sh
+++ research_runs.sh
@@ -67,7 +67,7 @@ for spur_type in "date"; do
             for lora_rank in 1 16 32 64; do
                 for proportion in 0.5; do
                     for token_proportion in 0.1; do
-                        for training_steps in 5000; do
+                        for training_steps in 30000; do
 
                                 echo "Running with location=$location, lora_rank=$lora_rank, proportion=$proportion, seed=$seed"
 
