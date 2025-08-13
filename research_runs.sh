337bbaa054c1b991e8c5475f30090c55c6437c2b
marcel_mateos_salles@brown.edu <mmateoss@gpu2002.oscar.ccv.brown.edu>
2025-08-13 12:46:48 -0400

    newer runs

--- research_runs.sh
+++ research_runs.sh
@@ -74,10 +74,10 @@ fi
 
 for spur_type in "exclamation_test"; do
     for seed in 40; do
-        for location in "random"; do
+        for location in "end"; do
             for lora_rank in 1 16 32 64; do
-                for proportion in 0.5; do
-                    for token_proportion in 0 0.1; do
+                for proportion in 0.25 0.5 0.75 1; do
+                    for token_proportion in 0; do
 
                         echo "Running with location=$location, lora_rank=$lora_rank, proportion=$proportion, seed=$seed"
 
